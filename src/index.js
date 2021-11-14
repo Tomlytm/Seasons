9c47a9ddda670e3117bcdd5bf8e254b2f33d7145
Tomilayo Jesse <60409841+Tomlytm@users.noreply.github.com>
2021-11-13 18:39:30 -0800

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import React from 'react'
+import ReactDom from 'react-dom'
+
+const App =()=> {
+    return (
+        <div>
+            Seasons
+        </div>
+    )
+
+}
+ReactDom.render(<App />, document.querySelector('#root'))
